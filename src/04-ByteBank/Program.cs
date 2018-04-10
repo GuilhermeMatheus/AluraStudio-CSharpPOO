225db54655f3981d36e80d572b5ce4e54c8c5ec5
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-10 19:53:19 -0300

    Video 3.3

--- src/04-ByteBank/Program.cs
+++ src/04-ByteBank/Program.cs
@@ -11,19 +11,38 @@ namespace _04_ByteBank
         static void Main(string[] args)
         {
             ContaCorrente contaDoBruno = new ContaCorrente();
-
+            
             contaDoBruno.titular = "Bruno";
-
+            
             Console.WriteLine(contaDoBruno.saldo);
-
+            
             bool resultadoSaque = contaDoBruno.Sacar(500);
-
+            
             Console.WriteLine(resultadoSaque);
             Console.WriteLine(contaDoBruno.saldo);
              
             contaDoBruno.Depositar(500); 
-            Console.WriteLine(contaDoBruno.saldo); 
-             
+            Console.WriteLine(contaDoBruno.saldo);
+            
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            
+            contaDaGabriela.titular = "Gabriela";
+            
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
+            
+            bool resultadoTransferencia = contaDoBruno.Transferir(2000, contaDaGabriela);
+            
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
+
+            Console.WriteLine("Resultado transferência: " + resultadoTransferencia);
+
+            contaDaGabriela.Transferir(100, contaDoBruno);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
+
             Console.ReadLine();
         }
     }
