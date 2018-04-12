17f2e8e678b72752b4691cae8b7fff5f0b46d8d6
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-12 17:54:07 -0300

    Video 6.2

--- src/07-ByteBank/ContaCorrente.cs
+++ src/07-ByteBank/ContaCorrente.cs
@@ -6,6 +6,9 @@ namespace _07_ByteBank
     {
         public Cliente Titular { get; set; }
 
+        public static int TotalDeContasCriadas { get; private set; }
+
+
         private int _agencia;
         public int Agencia
         {
@@ -49,6 +52,8 @@ namespace _07_ByteBank
         {
             Agencia = agencia;
             Numero = numero;
+
+            TotalDeContasCriadas++;
         }
 
 
